8d36282bd437c9e4a8d562a607d991813dbaded8
Kasra Babazadeh Mahalleh <164660956+kasrababazadeh@users.noreply.github.com>
2026-06-03 22:56:28 +0200

    Delete frontend-backup-files directory

--- frontend-backup-files/src/hooks/useAuth.ts
+++
@@ -1,6 +0,0 @@
-import { useAuthContext } from '../contexts/AuthContext';
-
-export default function useAuth() {
-const { user, login, logout } = useAuthContext();
-return { user, login, logout };
-}
--- frontend-backup-files/src/hooks/useRTL.ts
+++
@@ -1,8 +0,0 @@
-import { useEffect } from 'react';
-
-export default function useRTL() {
-useEffect(() => {
-document.documentElement.setAttribute('dir', 'rtl');
-document.documentElement.setAttribute('lang', 'fa');
-}, []);
-}
